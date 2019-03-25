7e67e6ab734a3efbfb2439da334c1a3f00341f1c
DANIEL ELIAS JIMENEZ ROJAS <1084357@est.intec.edu.do>
2019-03-24 21:30:08 -0400

    Cree las clases: Usuario, Profesor : Usuario, Estudiante : Usuario y Asignatura.

---
+++ Sistema de Calculo de Indice/Sistema de Calculo de Indice/LogIn.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Sistema_de_Calculo_de_Indice
+{
+    partial class LogIn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLogIn = new System.Windows.Forms.Button();
+            this.tbUser = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnLogIn
+            // 
+            this.btnLogIn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogIn.Location = new System.Drawing.Point(81, 291);
+            this.btnLogIn.Name = "btnLogIn";
+            this.btnLogIn.Size = new System.Drawing.Size(313, 53);
+            this.btnLogIn.TabIndex = 0;
+            this.btnLogIn.Text = "Iniciar Sesion";
+            this.btnLogIn.UseVisualStyleBackColor = true;
+            // 
+            // tbUser
+            // 
+            this.tbUser.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbUser.Location = new System.Drawing.Point(108, 103);
+            this.tbUser.Name = "tbUser";
+            this.tbUser.Size = new System.Drawing.Size(255, 20);
+            this.tbUser.TabIndex = 1;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbPassword.Location = new System.Drawing.Point(108, 166);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(255, 20);
+            this.tbPassword.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Administrador",
+            "Estudiante",
+            "Profesor"});
+            this.comboBox1.Location = new System.Drawing.Point(108, 228);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(255, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // LogIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 390);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbUser);
+            this.Controls.Add(this.btnLogIn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LogIn";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLogIn;
+        private System.Windows.Forms.TextBox tbUser;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Sistema de Calculo de Indice/Sistema de Calculo de Indice/Usuarios.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_de_Calculo_de_Indice
+{
+    class Usuarios
+    {
+        int id;
+        string nombre;
+        string apellido;
+        string constraseña;
+
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public string Constraseña { get => constraseña; set => constraseña = value; }
+    }
+}
